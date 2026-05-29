6ae48d9230ee88164a9968937c39588ffcc61374
santhosh-103 <santhosh00cse@gmail.com>
2026-05-29 18:19:13 +0530

    fix code execution api

--- client/src/api/codeApi.js
+++ client/src/api/codeApi.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const API_URL =
+  import.meta.env.VITE_API_URL;
+
 export const runCode = (data) =>
   axios.post(
-    "http://localhost:5000/api/code/run",
+    `${API_URL}/api/code/run`,
     data
   );
